1e03ed0f53e70af6620682804e3a41c975dea249
Heath Blandford <heath.blandford@gmail.com>
2020-11-22 22:06:30 -0600

    bump

--- pages/index.js
+++ pages/index.js
@@ -63,7 +63,7 @@ export default function IndexPage() {
                 type="text"
                 id="username"
                 className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-                placeholder="username"
+                placeholder="whoisheath"
               />
             </div>
             <label
